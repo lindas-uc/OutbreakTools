fbaf377bb335ab772c66ac6423b99cf3edf64ba1
Janik Endtner <janik.endtner@iwi.unibe.ch>
2016-03-15 17:09:57 +0100

    added some features in input.

--- js/lindasMain.js
+++ js/lindasMain.js
@@ -1,1 +1,54 @@
 var app = angular.module('lindasMain', []);
+
+app.service('sparql', function() {
+
+    var sparqlQuery = {
+        endpoint: "http://test.lindas-data.ch/sparql",
+        prefixBlv: "<http://blv.ch/>",
+        prefixXsd: "<http://www.w3.org/2001/XMLSchema#>",
+        from: "<http://test.lindas-data.ch/resource/animaltransports>",
+
+        sparql: "PREFIX blv: <http://blv.ch/>PREFIX xsd:<http://www.w3.org/2001/XMLSchema#>SELECT DISTINCT ?T ?D FROM <http://test.lindas-data.ch/resource/animaltransports>WHERE {?M blv:fromFarm <http://foodsafety.data.admin.ch/business/51122>.?M blv:toFarm ?T.?M blv:date ?D.}"
+    }
+
+    this.executeSparql = function () {
+        function exec() {
+            /* Uncomment to see debug information in console */
+            d3sparql.debug = true
+            d3sparql.query(sparqlQuery.endpoint, sparqlQuery.sparql, render)
+        }
+        function render(json) {
+            return json;
+            /* set options and call the d3spraql.xxxxx function in this library ... */
+            /*var config = {
+             "selector": "#result"
+             }
+             d3sparql.htmltable(json, config);
+             console.log(d3sparql.htmltable(json, config));*/
+        }
+
+        console.log("start execution");
+        exec();
+    }
+
+    this.setEndpoint = function(endpoint) {
+        sparqlQuery.endpoint = endpoint;
+    }
+
+    this.setSparql = function(sparql) {
+        sparqlQuery.sparql = sparql;
+    }
+});
+
+app.service('validator', function() {
+    this.validateDate = function(date) {
+        var now = new Date();
+        var valDate = moment(date, "DD/MM/YYYY").toDate();
+        console.log((now.getDate() - valDate.getDate()));
+        if ((now.getDate() - moment(date, "DD/MM/YYYY")) <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+});
--- js/lindasMainCtrl.js
+++ js/lindasMainCtrl.js
@@ -1,23 +1,46 @@
-app.controller('lindasMainCtrl', function($scope) {
+app.controller('lindasMainCtrl', function($scope, sparql, validator) {
     //tie => tested infected entities
     $scope.tieIds = new Array(1);
     $scope.startDate = "";
     $scope.endDate = "";
     $scope.value = "initialize";
     $scope.invalidIds = false;
-    $scope.invalidStartOrEndDate = false;
+    $scope.difference = 0;
+    $scope.dateInvalid = false;
 
     $scope.addEmptyTieIds = function() {
         $scope.tieIds.push(null);
-    }
+    };
 
     $scope.setStartDate = function(startDate) {
         $scope.startDate = startDate;
-    }
+    };
 
     $scope.setEndDate = function(endDate) {
         $scope.endDate = endDate;
-    }
+    };
+
+    $scope.$watch('startDate', function() {
+        if ((validator.validateDate($scope.startDate)) || (validator.validateDate($scope.endDate)))
+            $scope.dateInvalid = true;
+        else
+            $scope.dateInvalid = false;
+    });
+
+    $scope.$watch('endDate', function() {
+        if ((validator.validateDate($scope.startDate)) || (validator.validateDate($scope.endDate)))
+            $scope.dateInvalid = true;
+        else
+            $scope.dateInvalid = false;
+    });
+
+    $scope.$watch('difference', function() {
+        if ($scope.difference == 0)
+            return null;
+        var d = moment($scope.endDate, 'DD/MM/YYYY').toDate();
+        d.setDate(d.getDate()-$scope.difference);
+        $scope.startDate = moment(d).format('DD/MM/YYYY');
+    });
 
     $scope.initializeVisualisation = function() {
         console.log("start Visualization");
@@ -29,21 +52,22 @@ app.controller('lindasMainCtrl', function($scope) {
                 validId = true;
             }
         });
+
         if (!validId) {
             $scope.invalidIds = true;
         } else
             $scope.invalidIds = false;
 
-        if ($scope.startDate == "" || $scope.endDate == "") {
-            $scope.invalidStartOrEndDate = true;
-        } else
-            $scope.invalidStartOrEndDate = false;
-
-        if ($scope.invalidIds || $scope.invalidStartOrEndDate)
+        if (!validator.validateDate(startDate) || !validator.validateDate(endDate)) {
+            $scope.dateInvalid = true;
             return null;
+        } else
+            $scope.dateInvalid = false;
 
-        console.log($scope.startDate);
-        console.log($scope.tieIds)
+        //execute sparql query
+        var result = sparql.executeSparql();
+        console.log(result);
+
 
     }
 });
